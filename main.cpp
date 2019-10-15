0b01a6c4ff2525619793ab4351c92cc91767b382
Harish Krupo <harishkrupo@gmail.com>
2019-10-14 21:19:06 -0400

    main: few test cases
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ int main(int argc, char** argv) {
 	struct BTree *tree = NULL;
 
 	tree = BTree_create(2);
+	std::cout << BTree_print(tree) << std::endl;
+
 	for (int i = 0; i < 10; i++) {
 		BTree_insert(tree, i, NULL);
+		std::cout << "inserted key " << std::to_string(i) << ": " << BTree_print(tree) << std::endl;
 	}
 
 	return 0;
